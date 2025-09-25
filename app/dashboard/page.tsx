272a640833cc660978b466db45caaed00a62a7f3
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-09-25 10:17:43 +0100

    middleware for redirects to authenticate

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,4 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
-
 export default async function DashboardRoute() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user) {
-    return redirect("/api/auth/register");
-  }
   return (
     <div>
       <h1>Hello from the dashboard</h1>
