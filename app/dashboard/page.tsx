2417c8e0c86c89021afe0851e234cc1f1313996e
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-09-26 18:16:21 +0100

    UI for created post

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,26 @@
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { prisma } from "../utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { BlogPostCard } from "@/components/general/BlogPostCard";
 
+async function getData(userId: string) {
+  const data = await prisma.blogPost.findMany({
+    where: {
+      authorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
 export default async function DashboardRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const data = await getData(user.id);
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
@@ -10,6 +29,12 @@ export default async function DashboardRoute() {
         <Link className={buttonVariants()} href="/dashboard/create">
           Create Post
         </Link>
+      </div>
+
+      <div className="grid grid-col md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <BlogPostCard data={item} key={item.id} />
+        ))}
       </div>
     </div>
   );
