f4d931df9e91ccc4bae38ec327d35495f9bd0e2d
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-09-26 18:27:22 +0100

    dashboard work done

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,5 +1,5 @@
 import { handleSubmission } from "@/app/actions";
-import { Button } from "@/components/ui/button";
+import { SubmitButton } from "@/components/general/SubmitButton";
 import {
   Card,
   CardContent,
@@ -37,7 +37,7 @@ export default function CreateBlogRoute() {
               <Input name="url" required type="url" placeholder="Image url" />
             </div>
 
-            <Button>Create Post</Button>
+            <SubmitButton />
           </form>
         </CardContent>
       </Card>
